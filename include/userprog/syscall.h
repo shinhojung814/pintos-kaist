572377052f0a98733b40b45fd10803e977f5c8e9
shinhojung814 <shinhojung814@gmail.com>
2021-10-16 00:44:19 +0000

    system-calls

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -1,20 +1,9 @@
-// #ifndef USERPROG_SYSCALL_H
-// #define USERPROG_SYSCALL_H
-// #include <list.h>
-
-// void syscall_init(void);
-
-// struct lock file_lock;
-
-// #endif /* userprog/syscall.h */
-
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 #include <list.h>
 
 void syscall_init(void);
 
-// Project 2-4. File descriptor
-struct lock file_lock; // prevent simultaneous read, write (race condition prevention?)
+struct lock file_lock;
 
 #endif /* userprog/syscall.h */
