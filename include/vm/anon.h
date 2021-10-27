2c5ec2f8da793a0195212c9c54bdfa587d477d44
shinhojung814 <shinhojung814@gmail.com>
2021-10-27 11:22:40 +0000

    swap-in/out

--- include/vm/anon.h
+++ include/vm/anon.h
@@ -14,18 +14,3 @@ void vm_anon_init(void);
 bool anon_initializer(struct page *page, enum vm_type type, void *kva);
 
 #endif
-
-// #ifndef VM_ANON_H
-// #define VM_ANON_H
-// #include "vm/vm.h"
-// struct page;
-// enum vm_type;
-
-// struct anon_page {
-//     int swap_index;
-// };
-
-// void vm_anon_init (void);
-// bool anon_initializer (struct page *page, enum vm_type type, void *kva);
-
-// #endif
