1f09ab3202dd75a1991c7e4d5cdfc26c826c8302
shinhojung814 <shinhojung814@gmail.com>
2021-10-27 11:23:59 +0000

    swap-in/out

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -26,34 +26,3 @@ bool lazy_load_segment(struct page *page, void *aux);
 bool setup_stack(struct intr_frame *if_);
 
 #endif /* userprog/process.h */
-
-// #ifndef USERPROG_PROCESS_H
-// #define USERPROG_PROCESS_H
-
-// #include "threads/thread.h"
-// #include "filesys/off_t.h"
-// #include <stdbool.h>
-
-// tid_t process_create_initd (const char *file_name);
-// tid_t process_fork (const char *name, struct intr_frame *if_);
-// int process_exec (void *f_name);
-// int process_wait (tid_t);
-// void process_exit (void);
-// void process_activate (struct thread *next);
-// void load_userStack(char **argv, int argc, void **rspp);
-// // for VM
-// bool install_page (void *upage, void *kpage, bool writable);
-// bool lazy_load_segment (struct page *page, void *aux);
-// // bool setup_stack (struct intr_frame *if_);
-// struct file *process_get_file(int fd);
-
-// // project3 | lazy load를 위해 필요한 정보 구조체
-// struct container {
-//     struct file *file;
-//     // bool writable;
-//     // size_t file_len;
-//     off_t offset;               // 읽어야 할 파일 오프셋
-//     size_t page_read_bytes;     // 가상 페이지에 쓰여져 있는 데이터 크기
-// };
-
-// #endif /* userprog/process.h */
